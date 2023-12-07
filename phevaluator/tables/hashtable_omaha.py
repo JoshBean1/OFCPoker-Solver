1fa00ca0372769b56e0fda3932782ca0b4f1ffce
Joshua Bean <jab1896@msstate.edu>
2023-12-07 12:10:12 -0600

    This breaks omaha hand evaluation (not necessary) so that an executable for game.py can be generated. Can't figure out how to integrate .dat files with pyinstaller.

--- phevaluator/tables/hashtable_omaha.py
+++ phevaluator/tables/hashtable_omaha.py
@@ -2,7 +2,12 @@
 from pathlib import Path
 from struct import unpack
 
+'''
 DIR = Path(__file__).parent
 
 FLUSH_OMAHA = unpack("<4099095h", open(DIR / "omaha_flush.dat", "rb").read())
 NO_FLUSH_OMAHA = unpack("<11238500h", open(DIR / "omaha_noflush.dat", "rb").read())
+'''
+
+FLUSH_OMAHA = []
+NO_FLUSH_OMAHA = []
